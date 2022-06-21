be9fd472785e81b1c65f1b2fd6369a69fe5bfe8d
Dawitlior <dawitlior777@gmail.com>
2022-06-21 23:29:38 +0300

    add

--- main.js
+++ main.js
@@ -16,42 +16,42 @@ getNumbersAndSort([20, 14, 100, 3, 12, 67, 54, 3]);
 
 // // 3.	כתבו פונקציה המקבלת מערך של מספרים וממינת אותו מהגדול לקטן.
 
-// function getNumberAndSort(number) {
-//   console.log(number.sort((a , b) => b - a));
-// }
-// getNumberAndSort([2, 43, 1, 3, 65, 57, 43, 22]);
+function getNumberAndSort(number) {
+  console.log(number.sort((a , b) => b - a));
+}
+getNumberAndSort([2, 43, 1, 3, 65, 57, 43, 22]);
 
 // 4.	צרו פונקציה היוצרת מערך של 10 שמות פרטיים מהכיתה, השמות מתקבלים מהמשתמש, וסדרו אותו אלפביתית.
 
-// let classStudents = [];
-// let studentName = document.getElementById("student_name");
-// function getArrayFromUser() {
-//   if (classStudents.length < 10) {
-//     classStudents.push(studentName.value);
-//   }
-//   studentName.value = "";
-//   console.log(classStudents.sort());
-// }
+let classStudents = [];
+let studentName = document.getElementById("student_name");
+function getArrayFromUser() {
+  if (classStudents.length < 10) {
+    classStudents.push(studentName.value);
+  }
+  studentName.value = "";
+  console.log(classStudents.sort());
+}
 
 // 5.	צרו פונקציה היוצרת מערך רנדומלי של 10 גילים וסדרו אותו לפי סדר יורד.
 
-// let ageArray = [];
-// function getRandomAgeAndCreate(){
-// for(let i = 0 ; i<10 ; i++){
-//     ageArray.push(Math.floor(Math.random()*(65-18)+18))
-// }
-// ageArray.sort((a,b)=>{return a-b})
-// console.log(ageArray);
-// }
-// getRandomAgeAndCreate();
+let ageArray = [];
+function getRandomAgeAndCreate(){
+for(let i = 0 ; i<10 ; i++){
+    ageArray.push(Math.floor(Math.random()*(65-18)+18));
+}
+ageArray.sort((a,b)=>{return a-b});
+console.log(ageArray);
+}
+getRandomAgeAndCreate();
 
 // 6.	צרו מחרוזת שמחזיקה את המערך החדש מתרגיל 24 ומפרידה בין האיברים במקף (-).
 
-// function createStringAndKeepTheArray() {
-//   let str = classStudents.join("-");
-//   console.log(str);
-// }
-// createStringAndKeepTheArray();
+function createStringAndKeepTheArray() {
+  let str = classStudents.join("-");
+  console.log(str);
+}
+createStringAndKeepTheArray();
 
 // 7.
 // NOT FINISH
